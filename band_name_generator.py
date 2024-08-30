4bbf7f38fc44ea176a9fae369d0dfb98695d66ad
Riskofpain <157302230+Riskofpain@users.noreply.github.com>
2024-08-30 17:59:08 +0200

    Create band_name_generator.py

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+print("To jest super generator nazwy zespolu i jest bez sensu")
+pet = input("Jak ma na imie Twoje zwierzatko?")
+miasto = input("Z jakiego miasta jestes?")
+
+print("tylko nie z " + miasto )
+
+print("no nic... Twoja nazwa zespołu to: " + " " + pet + " " + miasto + " " + "tak wiem ze to bez sensu ale takie bylo zadanie XD")
